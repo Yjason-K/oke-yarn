5a691fa245fae5e6f48b12e870fae3001b955108
김영재 <gomgom0324@gmail.com>
2024-02-27 13:06:53 +0900

    zustand 예제 코드

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {create} from "zustand";
+
+interface CounterState {
+    count: number;
+    increase: () => void;
+    decrease: () => void;
+}
+
+const useCounterStore = create<CounterState>((set) => ({
+    count: 0,
+    increase: () => set((state) => ({ count: state.count + 1 })),
+    decrease: () => set((state) => ({ count: state.count - 1 })),
+}));
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const count =useCounterStore((state) => state.count)
+  const increaseCount = useCounterStore(state => state.increase)
+  const [input, setInput] = useState('')
+  const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
   return (
     <>
       <div>
@@ -18,9 +35,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={increaseCount}>
           count is {count}
         </button>
+        <input value={input} onChange={inputChange}/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
