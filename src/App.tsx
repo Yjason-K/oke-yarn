eea8d7495dd61fe9193d067e2133be6ee5c863ef
김영재 <gomgom0324@gmail.com>
2024-02-27 16:53:51 +0900

    router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {Route, Routes} from "react-router-dom";
 import JH from "./components/JH.tsx";
 import JS from "./components/JS.tsx";
 import YJ from "./components/YJ.tsx";
+import SM from "./components/SM.tsx";
+import JY from "./components/JY.tsx";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/JH" element={<JH/>} />
       <Route path="/JS" element={<JS/>} />
       <Route path="/YJ" element={<YJ/>} />
+      <Route path="/SM" element={<SM/>} />
+      <Route path="/JY" element={<JY/>} />
     </Routes>
   )
 }
