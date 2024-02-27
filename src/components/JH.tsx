a349a636d2ed626c6af04dfbae251fe494ae54e1
곽지혜 <jh.kwak@okestro.com>
2024-02-27 16:14:23 +0900

    [라이브러리]axios+lodash 테스트

--- src/components/JH.tsx
+++ src/components/JH.tsx
@@ -1,7 +1,41 @@
-const JH = () => {
-  return (
-      <h3>지혜님</h3>
-  )
-}
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import _ from 'lodash';
 
-export default JH
+const JH = () => {
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/users');
+        const processedUsers = _.map(response.data, user => ({
+          ...user,
+          name: _.toUpper(user.name),
+        }));
+        setUsers(processedUsers);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+      <div>
+        <h3>지혜님</h3>
+        {users.length > 0 ? (
+            <ul>
+              {users.map(user => (
+                  <li key={user.id}>{user.name}</li>
+              ))}
+            </ul>
+        ) : (
+            <p>No users found</p>
+        )}
+      </div>
+  );
+};
+
+export default JH;
